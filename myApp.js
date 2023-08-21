508a3d70691511ebcb514e6073c098911edecf0f
Robert Corrado <jhello7@pm.me>
2023-08-20 21:27:58 -0400

    added a sendfile path, fixed error

--- myApp.js
+++ myApp.js
@@ -4,7 +4,7 @@ let app = express();
 let absolutePath = __dirname + "/views/index.html";
 
 app.get("/", function (req, res) {
-  rres.sendFile(absolutePath);
+  res.sendFile(absolutePath);
 });
 
 module.exports = app;
