d041c25f11ba0406d9a4aa63278aa692c9aed731
Robert Corrado <jhello7@pm.me>
2023-08-20 21:37:05 -0400

    added a public Path

--- myApp.js
+++ myApp.js
@@ -2,9 +2,12 @@ let express = require("express");
 let app = express();
 
 let absolutePath = __dirname + "/views/index.html";
+let publicPath = __dirname + "/public";
 
 app.get("/", function (req, res) {
   res.sendFile(absolutePath);
 });
+
+app.use(express.static(publicPath));
 
 module.exports = app;
